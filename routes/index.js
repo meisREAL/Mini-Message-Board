e0d1139bcf2874aac47839970ff1c4b62330ab15
meisReal <simas.cesnakauskas@gmail.com>
2022-10-29 11:36:26 +0300

    ability to create new message

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,15 @@ const messages = [
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini MessageBoard', messages: messages });
 });
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Mini MessageBoard' });
+});
+router.post('/new', function (req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.user;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
 
 module.exports = router;
